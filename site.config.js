ac8bf7b2b38f2b0c7b0a4646968fc42b661ec111
JarrodKane <kajarrod@gmail.com>
2021-08-14 16:36:47 +1000

    - Added notion id

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '693a04a6acd9483eb7cc825b3364b836',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jarrod Kane',
+  domain: 'JarrodKane.com',
+  author: 'Jarrod Kane',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'A blog site about Jarrod Kane a wannabe athelet and a frontend developer',
+  socialImageTitle: 'Jarrod Kane',
+  socialImageSubtitle: `It's your boy Jarrod!`,
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: 'JarrodKane',
+  linkedin: 'jarrodkane',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
