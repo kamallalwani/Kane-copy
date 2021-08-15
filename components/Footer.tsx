18f6ee95f583dabd8fc16605c40d9cce7a0f60de
JarrodKane <kajarrod@gmail.com>
2021-08-15 14:45:34 +1000

    -minor styling modifications

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,6 +6,8 @@ import * as config from 'lib/config'
 
 import styles from './styles.module.css'
 
+import { SocialLinks } from './SocialLinks'
+
 // TODO: merge the data and icons from PageSocial with the social links in Footer
 
 export const Footer: React.FC<{
@@ -42,89 +44,7 @@ export const Footer: React.FC<{
       ) : null}
 
       <div className={styles.social}>
-        {config.twitter && (
-          <a
-            className={styles.twitter}
-            href={`https://twitter.com/${config.twitter}`}
-            title={`Twitter @${config.twitter}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaTwitter />
-          </a>
-        )}
-
-         {config.twitch && (
-          <a
-            className={styles.twitter}
-            href={`https://www.twitch.tv/${config.twitch}`}
-            title={`Twitch @${config.twitch}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaTwitch />
-          </a>
-        )}
-
-        {config.letterboxd && (
-          <a
-            className={styles.twitter}
-            href={`https://letterboxd.com/${config.letterboxd}`}
-            title={`Letterboxd @${config.letterboxd}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <SiLetterboxd />
-          </a>
-        )}
-
-{config.instagram && (
-          <a
-            className={styles.twitter}
-            href={`https://www.instagram.com/${config.instagram}`}
-            title={`Instagram @${config.instagram}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaInstagram />
-          </a>
-        )}
-
-{config.goodreads && (
-          <a
-            className={styles.twitter}
-            href={`https://www.goodreads.com/user/show/${config.goodreads}`}
-            title={`Goodreads @${config.goodreads}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGoodreads />
-          </a>
-        )}
-
-        {config.github && (
-          <a
-            className={styles.github}
-            href={`https://github.com/${config.github}`}
-            title={`GitHub @${config.github}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGithub />
-          </a>
-        )}
-
-        {config.linkedin && (
-          <a
-            className={styles.linkedin}
-            href={`https://www.linkedin.com/in/${config.linkedin}`}
-            title={`LinkedIn ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaLinkedin />
-          </a>
-        )}
+        <SocialLinks />
       </div>
     </footer>
   )
