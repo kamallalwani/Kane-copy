070de526059d1ebd1a7fc97f1b6219ba5bf68e9c
JarrodKane <kajarrod@gmail.com>
2021-08-17 20:56:18 +1000

    - Added back comment repo

--- site.config.js
+++ site.config.js
@@ -36,7 +36,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: "/blog-coments",
+  utterancesGitHubRepo: "JarrodKane/blog-coments",
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
